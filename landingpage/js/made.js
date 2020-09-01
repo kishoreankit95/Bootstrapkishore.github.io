6a9d92605da8f9efa603fc0820bfcfa21513e5c1
Ankit Kishore <kishore.ankit95@gmail.com>
2020-09-02 04:14:02 +0530

    updates

--- landingpage/js/made.js
+++ landingpage/js/made.js
@@ -1,13 +1,6 @@
-$(document).ready(function () {
-  
-  // Bottom scroll sync when top slided
-  $('.top-row').on("scroll", function () {
-      $('.btm-row').scrollLeft($(this).scrollLeft());
-  });
+$(document).ready(function(){
 
-  // Top scroll sync when bottom slided
-  $('.btm-row').on("scroll", function() {
-    $('.top-row').scrollLeft($(this).scrollLeft());
-  });
+  $('.modal').modal();
+
   
 });
