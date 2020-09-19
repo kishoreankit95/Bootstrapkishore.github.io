f59780e78bc5f447a2111f3b236b368257a41ee6
Ankit Kishore <kishore.ankit95@gmail.com>
2020-09-19 12:58:31 +0530

    weather demo

--- weatherapi/js/made.js
+++ weatherapi/js/made.js
@@ -1,31 +1,67 @@
 
 $(document).ready(function(){
 
+    $('#submitt').click(function (e) {
+
+        e.preventDefault();
+
+        var city = $('#city').val();
+
+        console.log(city);
+
+        var country = $('#country').val();
+
+        
+
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&units=Metric&APPID=eaeddcf19cf77bb3b515062fed6d2a73", function (data) {
+
+            console.log(data);
+
+            $('#apps span').empty();
+          
+            
+           
+
+            $('.temp span').append(Math.ceil(data.main.temp));
+
+            $('.humi span').append(Math.ceil(data.main.humidity));
+
+            $('.weth span').append(data.weather[0].main);
+
+           
+
+        });
+    });
+
+    
+        
+
+
+    // wetherinfo();
+
+    
   
   
 
 });
 
-$(document).ready(function() {
-  /* API: https://data.gov.in/resources/real-time-air-quality-index-Delhi-location */
-      $.get( "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd000001db984e68ef134d2740769e17c1894d1b&format=json&offset=0&limit=10000", function( data ) {
-        // $.get( "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd000001db984e68ef134d2740769e17c1894d1b&format=json&offset=0&limit=1000&filters[state]=West_Bengal",function( data ) {
-        if(data.status==="ok") {
-          $("#api-title").append(data.title);
-          $("#lastUpdated").append('Last Updated On ' + data.updated_date);
-          /* looping through the values and appending them to the table */
-          $.each(data.records, function(i, item) {
-              $('#air-quality-table > tbody:last-child').append('<tr><td>'+ data.records[i].id+'</td><td>'+ data.records[i].city+'</td><td>'+ data.records[i].station+'</td><td>'+ data.records[i].pollutant_id+'</td><td>'+ data.records[i].pollutant_min+'</td><td>'+ data.records[i].pollutant_max+'</td><td>'+ data.records[i].pollutant_avg+'</td></tr>');
-          });
-          }
-          /*Initializing Datatables*/
-          $('#air-quality-table').DataTable();
-      }, "json" );
-      $("#air-quality-table").on("draw.dt", function () {
-          $(this).find(".dataTables_empty").parents('tbody').empty();
-      });
+// function weatherInfo () {
+//     var city = $('#city').val();
 
-      
-  });
-  /* Hiding No data text on data tables since data is append in jquery on DOM */
+//     console.log(city);
+
+//     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=Metric&APPID=eaeddcf19cf77bb3b515062fed6d2a73", function (data) {
+
+//         console.log(data);
+
+
+//         $('.temp').append(data.main.temp);
+
+//         $('.humi').append(data.main.humidity);
+
+//         $('.weth').append(data.weather[0].main);
+
+//     });
+// }
+
 
