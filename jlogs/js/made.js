bdee2e253da4f76bb803b51167dc38758ffa544f
Ankit Kishore <kishore.ankit95@gmail.com>
2020-12-25 17:10:28 +0530

    jlog file add

---
+++ jlogs/js/made.js
@@ -0,0 +1,24 @@
+
+//Split text Section -- 1
+var entrtxt = document.getElementById("entrtxt");
+
+var entrgap = document.getElementById("entrgap");
+
+var splittext = document.getElementsByClassName("split__text");
+
+console.log(entrgap);
+console.log(splittext);
+
+function entrtxtCopy () {
+  var splittxtval = entrtxt.value;
+  var splittxtat = entrgap.value;
+
+  console.log("Hi");
+
+  var result = splittxtval.split(splittxtat);
+
+  for (var i=0; i<splittext.length; i++) {
+    splittext[i].innerHTML = result;
+  }
+  
+}
