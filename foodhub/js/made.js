b084f9befcf5e8e82832522972d6f8a50e806576
Ankit Kishore <kishore.ankit95@gmail.com>
2021-01-02 21:07:58 +0530

    add styles

--- foodhub/js/made.js
+++ foodhub/js/made.js
@@ -14,8 +14,125 @@ $(document).ready(function(){
     console.log("hii");
   }
   
+  $(".on-off").on("click", shiftdot);
+
+  function shiftdot() {
+    $(this).toggleClass("switch");    
+  }
+
+  $(".header .header__icons a").on("click", addclickeff);
+
+  function addclickeff () {
+    $(".header .header__icons a").removeClass("active");
+    $(this).addClass("active");
+  }
+
+  var button;
+
+  $(".btn-addv").on("click", incvalu);
+
+  function incvalu () {
+    var capinp = $(this).siblings(".btn-totv");
+    var capini = capinp.val();
+    var incval = ++capini;
+    capinp.val(incval);
+    // console.log(capini); 
+  }
+
+  $(".btn-decv").on("click", decvalu);
+
+  function decvalu () {
+    var capinp = $(this).siblings(".btn-totv");
+    var capini = capinp.val();
+    var incval = --capini;
+    if(capini <= 0) {
+      capinp.val(0);
+    }
+    else{
+      capinp.val(incval);
+    } 
+  }
+
+
+  // Home page Tab input button
+  $("#home .btn-totv").on("change", addfooditem);
+
+  var target1 = $("#drinks");
+
+  function addfooditem () {
+    var inpval = $(this).val();
+
+    var itmname = $(this).parent().siblings("h4").text();
+    var itmuprice = $(this).parent().siblings("p").children(".unit-pric").text();
+
+    var totcost = itmuprice*inpval;
+    var totcostPre = totcost.toPrecision(3);
+    var netprice = parseFloat(totcostPre);
+    
+    var categTotalini = $("#drinks .categ-total").text();
+
+    if (inpval > 0) {
+      const adddiv = document.createElement("div");
+
+      adddiv.className = "sep-items";
+
+      adddiv.innerHTML = `
+      <p class="fd__name">` + itmname + `</p>
+      <p class="fd__brkup">` + "$" + itmuprice + ` x ` + inpval + `</p>
+      <p class="fd__tot">`+ "$" + netprice + `</p> `;
+
+      target1.append(adddiv);
+
+      // categTotal.text(netprice);
+      
+    }
+
+    var categTotalNum = parseFloat(categTotalini);
+    var categTotal = categTotalNum + netprice;
+
+    $("#drinks .categ-total").text(categTotal);
+
+    // console.log("ck");
+    return netprice;
+  }
+
+
+  $("#home .btn-addv").on("click", checkfunc1);
   
-  
+  function checkfunc1 () {
+
+    var tempstr1 = $(this).siblings(".btn-totv").val();
+    console.log(typeof(tempstr1));
+
+    var itmname = $(this).parent().siblings("h4").text();
+    var itmuprice = $(this).parent().siblings("p").children(".unit-pric").text();
+
+    var totcost = itmuprice*tempstr1;
+    var totcostPre = totcost.toPrecision(3);
+    var netprice = parseFloat(totcostPre);
+
+    if (tempstr1 > 0) {
+      const adddiv = document.createElement("div");
+
+      adddiv.className = "sep-items";
+
+      adddiv.innerHTML = `
+      <p class="fd__name">` + itmname + `</p>
+      <p class="fd__brkup">` + "$" + itmuprice + ` x ` + tempstr1 + `</p>
+      <p class="fd__tot">`+ "$" + netprice + `</p> `;
+
+      target1.append(adddiv);
+
+      // categTotal.text(netprice);
+      
+    }
+
+
+  }
+
+
 
 });
+
+
 
