3212abf2dee8f1e56de1fbf8528175ee461925d7
Ankit Kishore <kishore.ankit95@gmail.com>
2021-01-03 12:37:05 +0530

    js

--- foodhub/js/made.js
+++ foodhub/js/made.js
@@ -58,18 +58,27 @@ $(document).ready(function(){
   $("#home .btn-totv").on("change", addfooditem);
 
   var target1 = $("#drinks");
+  
 
   function addfooditem () {
     var inpval = $(this).val();
 
+    //Picking text from item menu
     var itmname = $(this).parent().siblings("h4").text();
     var itmuprice = $(this).parent().siblings("p").children(".unit-pric").text();
 
+    // Calculating costs for invoice breakup
     var totcost = itmuprice*inpval;
     var totcostPre = totcost.toPrecision(3);
     var netprice = parseFloat(totcostPre);
     
-    var categTotalini = $("#drinks .categ-total").text();
+
+    //Price change in Invoice section
+    // var categTotalini = $("#drinks .categ-total").text();
+
+
+    
+    
 
     if (inpval > 0) {
       const adddiv = document.createElement("div");
@@ -82,14 +91,58 @@ $(document).ready(function(){
       <p class="fd__tot">`+ "$" + netprice + `</p> `;
 
       target1.append(adddiv);
-
       
     }
 
-    var categTotalNum = parseFloat(categTotalini);
+
+    //Check if item already in invoice section
+    // var checkitem1 = $("#drinks .sep-items .fd__name").text();
+    // console.log(checkitem1);
+
+    // if(itmname == checkitem1){
+    //   console.log("Found");
+    // }
+
+    //Picking text from breakup headers of three regions
+    var categTotalini1 = $("#drinks .categ-total").text();
+    console.log(categTotalini1);
+    var categTotalini2 = $("#food .categ-total").text();
+    var categTotalini3 = $("#desert .categ-total").text();
+
+
+    var categTotalNum = parseFloat(categTotalini1);
     var categTotal = categTotalNum + netprice;
+    // console.log(categTotalNum);
+    // console.log(categTotal);
 
     $("#drinks .categ-total").text(categTotal);
+
+    // Targetting summary section
+    var choutbasic = $(".chkout-summ .chkout-sep p .chkout-basic");
+    // var choutbasictx = $(".chkout-summ .chkout-sep p .chkout-basic").text();
+    var chouttax = $(".chkout-summ .chkout-sep p .chkout-tax");
+    // var chouttaxtx = $(".chkout-summ .chkout-sep p .chkout-tax").text();
+    var choutdisc = $(".chkout-summ .chkout-sep p .chkout-disc");
+    // var choutdisctx = $(".chkout-summ .chkout-sep p .chkout-disc").text();
+    var chouttotl = $(".price-chkut .btn-hold .chkout-totl");
+    // var chouttotltx = $(".price-chkut .btn-hold .chkout-totl").text();
+
+    // console.log(choutbasic);
+    // console.log(categTotalini1);
+    // console.log(categTotalini2);
+    // console.log(categTotalini3);
+
+    var choutbasictx = parseFloat(categTotal) + parseFloat(categTotalini2) + parseFloat(categTotalini3);
+    var chouttaxtx = (choutbasictx*0.18).toPrecision(3);
+    var chouttaxtx4 = parseFloat(chouttaxtx);
+    var chouttotltx = (choutbasictx + chouttaxtx4);
+
+    // console.log(choutbasic);
+    // console.log(chouttax);
+
+    choutbasic.text(choutbasictx);
+    chouttax.text(chouttaxtx);
+    chouttotl.text(chouttotltx);
 
     // console.log("ck");
     return netprice;
@@ -100,6 +153,7 @@ $(document).ready(function(){
   $("#profile .btn-totv").on("change", addfooditem2);
 
   var target2 = $("#food");
+  // var categTotalini2 = $("#food .categ-total").text();
 
   function addfooditem2 () {
     var inpval = $(this).val();
@@ -111,7 +165,7 @@ $(document).ready(function(){
     var totcostPre = totcost.toPrecision(3);
     var netprice = parseFloat(totcostPre);
     
-    var categTotalini = $("#food .categ-total").text();
+    // var categTotalini = $("#food .categ-total").text();
 
     if (inpval > 0) {
       const adddiv = document.createElement("div");
@@ -129,7 +183,7 @@ $(document).ready(function(){
       
     }
 
-    var categTotalNum = parseFloat(categTotalini);
+    var categTotalNum = parseFloat(categTotalini2);
     var categTotal = categTotalNum + netprice;
 
     $("#food .categ-total").text(categTotal);
@@ -143,6 +197,7 @@ $(document).ready(function(){
   $("#contact .btn-totv").on("change", addfooditem3);
 
   var target3 = $("#desert");
+  // var categTotalini3 = $("#desert .categ-total").text();
 
   function addfooditem3 () {
     var inpval = $(this).val();
@@ -154,7 +209,7 @@ $(document).ready(function(){
     var totcostPre = totcost.toPrecision(3);
     var netprice = parseFloat(totcostPre);
     
-    var categTotalini = $("#desert .categ-total").text();
+    // var categTotalini = $("#desert .categ-total").text();
 
     if (inpval > 0) {
       const adddiv = document.createElement("div");
@@ -172,7 +227,7 @@ $(document).ready(function(){
       
     }
 
-    var categTotalNum = parseFloat(categTotalini);
+    var categTotalNum = parseFloat(categTotalini3);
     var categTotal = categTotalNum + netprice;
 
     $("#desert .categ-total").text(categTotal);
