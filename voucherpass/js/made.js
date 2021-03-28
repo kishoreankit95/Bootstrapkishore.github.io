98561ccdbd55413273dfa32d6f03dd4236b17035
Ankit Kishore <kishore.ankit95@gmail.com>
2021-03-28 19:44:05 +0530

    add

--- voucherpass/js/made.js
+++ voucherpass/js/made.js
@@ -2,15 +2,42 @@
 $(document).ready(function(){
 
   let inps = $(".phoneNo").val();
-  console.log(inps);
+
+  let getGift = $(".getGift");
+
+
   console.log(inps.length);
 
-  $(".getGift").on("click", checkMobile)
+  $(".phoneNo").on("change", checkMobile);
 
 
   function checkMobile () {
-    let inps = $(".phoneNo").val();
-    console.log("mobile");
+    
+    let inps0 = parseInt(inps.charAt(0));
+    console.log(inps0);
+
+    const arr = [9,8,7,6];
+    console.log(arr.includes(parseInt(inps0)));
+    if(inps.length != 10){
+      alert("Please check your number");
+    }
+    else 
+    if(!arr.includes(inps0)){
+      alert("Please enter correct number");
+      console.log("in");
+    }
+    else{
+      getGift.addClass("active");
+    }
+
+  }
+
+  $(getGift).on("click", storeMobile);
+
+  function storeMobile() {
+    localStorage.setItem("mobNo", inps);
+    // localStorage.removeItem("mobNo");
+    console.log(localStorage.getItem("mobNo"));
   }
 
 
