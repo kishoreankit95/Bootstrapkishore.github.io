fa79eb0ea7602d7fab50739937d92c65e452859e
kishoreankit95 <kishore.ankit95@gmail.com>
2022-12-22 19:22:57 +0530

    adding new changes

--- jlogs/js/made.js
+++ jlogs/js/made.js
@@ -31,3 +31,26 @@ var rephold = document.getElementById("entrtxt2");
 
 var reptxt = document.getElementById("entrgap2");
 
+
+
+function arrayDiff() {
+
+  var stringDiffIndx1 = document.getElementById("stringDiffIndx1").value;
+  var stringDiffIndx2 = document.getElementById("stringDiffIndx2").value;
+  var stringDiffIndxRes = document.getElementById("stringDiffIndxRes");
+  
+
+  let strArray1 = stringDiffIndx1.split("");
+  let strArray2 = stringDiffIndx2.split("");
+  let index = null;
+
+  strArray1.forEach((elem, i) => {
+    if (!(elem === strArray2[i])) {
+      index = !index ? i : index;
+      console.log("elem ", elem);
+    }
+  });
+  console.log("index ", index);
+  stringDiffIndxRes.innerText = index;
+  
+}
